1de170ca97d5471dc2252e2efb10ce26480b3eb8
nunojrsilva <nunojosersilva@gmail.com>
2018-12-18 22:15:46 +0000

    Some modifications on frontend_state module

--- FrontEnd/frontend_state.erl
+++ FrontEnd/frontend_state.erl
@@ -1,16 +1,21 @@
 -module(frontend_state).
--export([start/0, getPapel/1]).
+-export([start/0]).
 
 -include("ccs.hrl").
 
 %Neste modulo vao ser implementadas funcoes de comunicacao com exchange e gestao da logica dessa comunicacao
-% MapEstado :  empresa -> lista de objetos apostador (username, pid)
+% MapEstado :  empresa -> {Socket da Exchange, Leilao, Emissao, UltimaTaxa, UtilizadoresInteressados = []}
 % MapUser : username -> papel
+
+
+
+%Falta ligar aos sockets da exchange e atualizar ultima taxa, se for para manter isso aqui
 
 
 start() ->
   io:format("State ja esta a correr!"),
-  register ( frontend_state, spawn (fun() -> loop ( #{"emp1" => "empresa", "emp2" => "empresa", "cli1" => "cliente", "cli2" => "cliente"}, #{}) end ))
+  %Ligacao aos sockets da exchange
+  loop ( #{"emp1" => "empresa", "emp2" => "empresa", "cli1" => "cliente", "cli2" => "cliente"}, #{})
 .
 
 loop (MapUser, MapEstado) -> 
@@ -29,23 +34,88 @@ loop (MapUser, MapEstado) ->
                     loop(MapUser, MapEstado)
                 
             end
+        ;
+        {licitacao, Empresa, User, From, ProtoBufBin}->
+            io:format("Loop recebeu licitacao ~n"),
+            case maps:find(Empresa, MapEstado) of
+                {ok, {SockExch, Leilao, Emissao, UltimaTaxa, UtilizadoresInteressados }} when Leilao == true -> 
+                    NovaLista = [{User, From} | UtilizadoresInteressados],
+                    NewMap = maps:put(Empresa, {SockExch, Leilao, Emissao, UltimaTaxa, NovaLista}, MapEstado),
+                    gen_tcp:send(SockExch, ProtoBufBin),
+                    loop(MapUser, NewMap)
+                ;
+                _-> 
+                    From ! invalid,
+                    loop(MapUser, MapEstado)
+            end
+        ;
+        {emissao, Empresa, User, From, ProtoBufBin}->
+            io:format("Loop recebeu emissao ~n"),
+            case maps:find(Empresa, MapEstado) of
+                {ok, {SockExch, Leilao, Emissao, UltimaTaxa, UtilizadoresInteressados }} when Emissao == true -> 
+                    NovaLista = [{User, From} | UtilizadoresInteressados],
+                    NewMap = maps:put(Empresa, {SockExch, Leilao, Emissao, UltimaTaxa, NovaLista}, MapEstado),
+                    gen_tcp:send(SockExch, ProtoBufBin),
+                    loop(MapUser, NewMap)
+                ;
+                _-> 
+                    From ! invalid,
+                    loop(MapUser, MapEstado)
+            end
+        ;
+        {iniciaLeilao, Empresa, From, ProtoBufBin}->
+            io:format("Loop recebeu iniciaLeilao ~n"),
+            case maps:find(Empresa, MapEstado) of
+                {ok, {SockExch, Leilao, Emissao, UltimaTaxa, _}} when Leilao == false -> 
+                    NovaLista = [],
+                    NewMap = maps:put(Empresa, {SockExch, true, Emissao, UltimaTaxa, NovaLista}, MapEstado),
+                    gen_tcp:send(SockExch, ProtoBufBin),
+                    loop(MapUser, NewMap)
+                ;
+                _-> 
+                    From ! invalid,
+                    loop(MapUser, MapEstado)
+            end
+        ;
+        {iniciaEmissao, Empresa, From, ProtoBufBin}->
+            io:format("Loop recebeu iniciaEmissao ~n"),
+            case maps:find(Empresa, MapEstado) of
+                {ok, {SockExch, Leilao, Emissao, UltimaTaxa, _}} when Emissao == false -> 
+                    NovaLista = [],
+                    NewMap = maps:put(Empresa, {SockExch, Leilao, true, UltimaTaxa, NovaLista}, MapEstado),
+                    gen_tcp:send(SockExch, ProtoBufBin),
+                    loop(MapUser, NewMap)
+                ;
+                _-> 
+                    From ! invalid,
+                    loop(MapUser, MapEstado)
+            end
+        
+
     end
 .
 
 
 
-rpc( Req ) ->
-    frontend_state ! Req,
-    receive
-        {frontend_state, Res } -> 
-            io:format("RPC recebeu ~p~n", [Res]),
-            Res
-    end
-.
+% rpc( Req ) ->
+%     frontend_state ! Req,
+%     receive
+%         {frontend_state, Res } -> 
+%             %io:format("RPC recebeu ~p~n", [Res]),
+%             Res
+%     end
+% .
 
-getPapel (User) ->
-    io:format("Dentro do getPapel do frontend_state"),
-    rpc ({getPapel, User, self()}) 
+% getPapel (User) ->
+%     io:format("Dentro do getPapel do frontend_state"),
+%     rpc ({getPapel, User, self()}) 
     
-.
+% .
+
+
+% licitacao (Valor, Empresa, User) ->
+%     io:format("Dentro do licitacao do frontend_state"),
+%     rpc ({licitacao, Valor, Empresa, User, self()}) 
+    
+% .
 
