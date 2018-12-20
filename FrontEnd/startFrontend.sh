e78fc8950430ab312321b1f80860749be16f3213
nunojrsilva <nunojosersilva@gmail.com>
2018-12-20 16:49:36 +0000

    Modify .gitignore and shell script to use zeromq with erlang

--- FrontEnd/startFrontend.sh
+++ FrontEnd/startFrontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Starts server
 ../../gpb/bin/protoc-erl -I. ccs.proto
+rebar3 compile
 erlc  -I ../../gpb/include ccs.erl
-echo "c(login_manager) . c(frontend) . c(frontend_client) . c(frontend_state) . frontend:start(). " | erl
+echo "c(login_manager) . c(frontend) . c(frontend_client) . c(frontend_state) . frontend:start(). " | erl -pa _build/default/lib/chumak/ebin -eval
