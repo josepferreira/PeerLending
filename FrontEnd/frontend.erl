feaa9117e73d39ae763402229eec41c3c212f1b9
nunojrsilva <nunojosersilva@gmail.com>
2019-01-16 23:11:50 +0000

    Update frontend.erl , Json reading almost working

--- FrontEnd/frontend.erl
+++ FrontEnd/frontend.erl
@@ -6,51 +6,73 @@
 
 %Função para criar um mapa: PidState -> [Empresa], para sabermos qual o ator de state que gere as operacoes daquela empresa
 
-mapaStateEmpresa(Map, Push, Pull) ->
+mapaStateEmpresa(Map, 0) ->
+    Map
+;
+
+mapaStateEmpresa(Map, NumExchanges) ->
+  Json = jiffy:decode(file:read_file("modelo.json")),
+  io:format("~s~n", Json),
+  {Push, Pull} = criaSocket(12351, 12350),
   PidStateA = frontend_state:start(Push, Pull),
   Map2 = maps:put(PidStateA, ["emp1", "emp2"], Map),
-  Map2
+  mapaStateEmpresa(Map2, NumExchanges - 1)
 .
+
+criaSocket(PortaPush, PortaPull) ->
+
+    %%%  Ligacao via zeromq à exchange
+
+    {ok, Push} = chumak:socket(push),
+
+    %12351
+    case chumak:connect(Push, tcp, "localhost", PortaPush) of
+        {ok, _} ->
+            io:format("Binding OK with Pid: ~p\n", [Push]);
+        {error, Reason} ->
+            io:format("Connection Failed for this reason: ~p\n", [Reason]);
+        X ->
+            io:format("Unhandled reply for bind ~p \n", [X])
+        end,
+    
+    {ok, Pull} = chumak:socket(pull),
+
+    %12350
+    case chumak:connect(Pull, tcp, "localhost", PortaPull) of
+        {ok, _BindPid} ->
+            io:format("Binding OK with Pid: ~p\n", [Pull]);
+        {error, Reason1} ->
+            io:format("Connection Failed for this reason: ~p\n", [Reason1]);
+        X1 ->
+            io:format("Unhandled reply for bind ~p \n", [X1])
+        end,
+
+    {Push, Pull}.
+
+ %% Fim da ligacao
+
 
 start() ->
     
   login_manager:start(),
+
   application:start(chumak),
 
-  %%%  Ligacao via zeromq à exchange
-
-  {ok, Push} = chumak:socket(push),
-
-  case chumak:connect(Push, tcp, "localhost", 12351) of
-      {ok, _} ->
-          io:format("Binding OK with Pid: ~p\n", [Push]);
-      {error, Reason} ->
-          io:format("Connection Failed for this reason: ~p\n", [Reason]);
-      X ->
-          io:format("Unhandled reply for bind ~p \n", [X])
-    end,
   
-  {ok, Pull} = chumak:socket(pull),
-
-  case chumak:connect(Pull, tcp, "localhost", 12350) of
-      {ok, _BindPid} ->
-          io:format("Binding OK with Pid: ~p\n", [Pull]);
-      {error, Reason1} ->
-          io:format("Connection Failed for this reason: ~p\n", [Reason1]);
-      X1 ->
-          io:format("Unhandled reply for bind ~p \n", [X1])
-    end,
-
- %% Fim da ligacao
-
 %% Inicializacao do frontendState, com os sockets de push e pull
-  MapState = mapaStateEmpresa(#{}, Push, Pull),
+  %MapState = mapaStateEmpresa(#{}, 1),
 
   io:format("Servidor de Frontend Principal ja esta a correr!~n"),
+  {ok, Ficheiro} = file:read_file("modelo.json"),
+  {Json} = jiffy:decode(Ficheiro),
+  Ut = proplists:get_value(<<"Utilizadores">>, Json),
+  io:fwrite("~p~n", [Ut])
 
-  {ok, LSock} = gen_tcp:listen(12345, [binary, {packet, 4}, {active, true}, {reuseaddr, true}]),
 
-  acceptor(LSock, MapState).
+%   {ok, LSock} = gen_tcp:listen(12345, [binary, {packet, 4}, {active, true}, {reuseaddr, true}]),
+
+%   acceptor(LSock, MapState)
+.
 
 acceptor(LSock, MapState) ->
   {ok, Sock} = gen_tcp:accept(LSock),
@@ -98,7 +120,6 @@ autenticaCliente(Sock, MapState) ->
                     gen_tcp:controlling_process(Sock, Pid),
                     io:format("O PID do processo criado é: ~p~n", [Pid])
                   ;
-                    %user(Sock, User, Pid);
 
 
                 invalid -> Bin = ccs:encode_msg(#'RespostaAutenticacao'{sucesso = false}),
@@ -106,33 +127,8 @@ autenticaCliente(Sock, MapState) ->
                     autenticaCliente(Sock, MapState)
               end
             ;
-          %          gen_tcp:send(Sock, "Autenticação inválida\n"),
       {tcp_closed,_} ->
             io:format("utilizador nao se autenticou nem registou e saiu~n",[]),
             true
-    end.
-
-
-% %Este ator vai receber a mensaggem e reenchaminhar para o respetivo ator ... Eu nao consegui com que fosse o outro a esperar pela mensagem ...
-% user(Sock, User, Pid) ->
-    
-%     receive
-%       {tcp,_,Msg} -> io:format("User: " ++ User ++ " autenticado com sucesso!\n"),
-%                    Pid ! {self(), Msg},
-%                    receive
-%                      {Pid, ok} -> io:format("Recebi um okay~n")
-%                    end,
-%                    user(Sock, User, Pid);
-%       {tcp_closed, _} ->
-%         Res = login_manager:logout(User),
-%         case Res of
-%           ok -> io:format("utilizador desautenticado~n",[]);
-%           _ -> io:format("algum erro de desautenticacao~n",[])
-%         end;
-%       {tcp_error, _, _} ->
-%         Res = login_manager:logout(User),
-%         case Res of 
-%           ok -> io:format("utilizador desautenticado~n",[]);
-%           _ -> io:format("algum erro de desautenticacao~n",[])
-%         end
-%     end.
+    end
+.
