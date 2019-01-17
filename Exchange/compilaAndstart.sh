59ddf3fef224e81f06fca82e95841a171ef9978f
josepferreira <zepedroferreira11@gmail.com>
2019-01-17 13:41:01 +0000

    Configurações estão realizadas, falta testar tudo agora

---
+++ Exchange/compilaAndstart.sh
@@ -0,0 +1,12 @@
+javac -cp .:\* exchange/Proposta.java
+javac -cp .:\* exchange/ExcecaoUltrapassado.java
+javac -cp .:\* exchange/ExcecaoFinalizado.java
+javac -cp .:\* exchange/Proposta.java
+javac -cp .:\* exchange/Emprestimo.java
+javac -cp .:\* exchange/Leilao.java
+javac -cp .:\* exchange/Emissao.java
+javac -cp .:\* exchange/Empresa.java
+javac -cp .:\* exchange/TerminaEmprestimo.java
+javac -cp .:\* exchange/EstruturaExchange.java
+javac -cp .:\* exchange/Exchange.java 
+./run exchange/Exchange $*
--- Exchange/startExchange.sh
+++ Exchange/startExchange.sh
@@ -1,12 +1,1 @@
-javac -cp .:\* exchange/Proposta.java
-javac -cp .:\* exchange/ExcecaoUltrapassado.java
-javac -cp .:\* exchange/ExcecaoFinalizado.java
-javac -cp .:\* exchange/Proposta.java
-javac -cp .:\* exchange/Emprestimo.java
-javac -cp .:\* exchange/Leilao.java
-javac -cp .:\* exchange/Emissao.java
-javac -cp .:\* exchange/Empresa.java
-javac -cp .:\* exchange/TerminaEmprestimo.java
-javac -cp .:\* exchange/EstruturaExchange.java
-javac -cp .:\* exchange/Exchange.java 
-./run exchange/Exchange exchange1.json
+./run exchange/Exchange $*
