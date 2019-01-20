c50e1d94fbdd66a3ca899c958e3901531b263890
nunojrsilva <nunojosersilva@gmail.com>
2019-01-20 21:31:06 +0000

    Modify dependencies shell script

--- downloadDependencies.sh
+++ downloadDependencies.sh
@@ -1,2 +1,7 @@
 git clone https://github.com/tomas-abrahamsson/gpb.git ../gpb
+cd ../gpb 
+make
+cd ../PeerLending
+wget https://s3.amazonaws.com/rebar3/rebar3 && chmod +x rebar3
+sudo mv rebar3 /usr/local/bin
 sudo apt-get install erlang-jiffy
